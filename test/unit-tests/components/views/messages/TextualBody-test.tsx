3426d53a8ff401d85a6338095775047b07383f5b
Bas Nijholt <bas@nijho.lt>
2026-02-06 17:22:27 -0800

    Fix MindRoom Jest regressions in code/render snapshots

--- test/unit-tests/components/views/messages/TextualBody-test.tsx
+++ test/unit-tests/components/views/messages/TextualBody-test.tsx
@@ -9,7 +9,7 @@ Please see LICENSE files in the repository root for full details.
 import React, { type ComponentProps } from "react";
 import { type MatrixClient, type MatrixEvent, PushRuleKind, type Room } from "matrix-js-sdk/src/matrix";
 import { mocked, type MockedObject } from "jest-mock";
-import { render, waitFor } from "jest-matrix-react";
+import { fireEvent, render, waitFor } from "jest-matrix-react";
 import { PushProcessor } from "matrix-js-sdk/src/pushprocessor";
 
 import {
@@ -315,7 +315,12 @@ describe("<TextualBody />", () => {
                 "<pre><code class=\"language-py\"># Python Program to calculate the square root\n\n# Note: change this value for a different result\nnum = 8 \n\n# To take the input from the user\n#num = float(input('Enter a number: '))\n\nnum_sqrt = num ** 0.5\nprint('The square root of %0.3f is %0.3f'%(num ,num_sqrt))\n</code></pre>\n",
             );
             const { container } = getComponent({ mxEvent: ev }, matrixClient);
-            await waitFor(() => expect(container.querySelector(".hljs-built_in")).toBeInTheDocument());
+            const expandButton = container.querySelector<HTMLButtonElement>(".mx_CollapsibleBlock_header");
+            expect(expandButton).not.toBeNull();
+            fireEvent.click(expandButton!);
+
+            await waitFor(() => expect(container.querySelector(".hljs-built_in")).not.toBeNull());
+            expect(container.querySelector(".hljs-built_in")).toBeInTheDocument();
             const content = container.querySelector(".mx_EventTile_body");
             expect(content).toMatchSnapshot();
         });
